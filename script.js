b8d47e83f60edae24c86b9ab1a361e4d2ae5f3f3
kevindwi246 <v8zsw7fh7t@privaterelay.appleid.com>
2026-08-01 15:32:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const photos = [
+"IMG_3482.jpeg",
+"IMG_3671.jpeg",
+"IMG_3712.jpeg",
+"IMG_1615.png"
+];
 
+let index = 0;
+
+const img = document.getElementById("slide");
+
+setInterval(() => {
+index++;
+
+if(index >= photos.length){
+index = 0;
+}
+
+img.src = photos[index];
+
+},4000);
